48de6e9b0204c0d24b0fa337a64da1cb3f0ca92b
Carlota Gordillo Álvarez <carlotagordillo2002@gmail.com>
2025-02-11 22:05:26 +0100

    11-2

--- EDA/aux_functions.py
+++ EDA/aux_functions.py
@@ -42,3 +42,90 @@ def analysis_data(df, name):
     print("-----------------------\n")
      
     print(df.dtypes)
+    
+
+def remove_outliers(data, col):
+    Q3 = np.quantile(data[col], 0.75)
+    Q1 = np.quantile(data[col], 0.25)
+    IQR = Q3 - Q1
+    
+    # Define bounds for the outliers
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    
+    print("Lower Bound:", lower_bound)
+    print("Upper Bound:", upper_bound)
+    
+    # Filter the DataFrame based on the condition
+    filtered_data = data[(data[col] > lower_bound) & (data[col] < upper_bound)]
+
+    return filtered_data
+
+
+def percentage_outliers(data, col):
+    Q3 = np.quantile(data[col], 0.75)
+    Q1 = np.quantile(data[col], 0.25)
+    IQR = Q3 - Q1
+    
+    # Define bounds for the outliers
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    
+    #print("Lower Bound:", lower_bound)
+    #print("Upper Bound:", upper_bound)
+    
+    # Filter the DataFrame based on the condition
+    filtered_data = data[(data[col] < lower_bound) | (data[col] > upper_bound)]
+
+    
+    percentage_outliers = (len(filtered_data) / len(data))*100
+    
+    percentage = f'The percentage of outliers in {col} is {percentage_outliers}\n'
+    
+    print(percentage)
+
+    return percentage_outliers
+
+def create_frequency_table(df,name):
+    
+    # control
+    frequency_table_control = df[name].value_counts()
+   
+    proportion_table_control = df[name].value_counts(normalize=True)
+    
+    
+    return frequency_table_control, proportion_table_control
+
+
+def piechart(df, name, threshold):
+    
+    frequency_table, proportion_table = create_frequency_table(df, name)
+
+    print(frequency_table)
+
+    #threshold = 0.001  # minimum percentage to show a region alone 
+
+    # the region with a percentage lower it will be agrupated in 'other' colum
+    frequency_table_sorted = frequency_table.sort_values(ascending=False)
+    total = frequency_table_sorted.sum()
+
+    frequency_table_grouped = frequency_table_sorted.copy()
+    frequency_table_grouped[frequency_table_sorted / total < threshold] = 0
+    frequency_table_grouped["Others"] = frequency_table_sorted[frequency_table_sorted / total < threshold].sum()
+
+    frequency_table_grouped = frequency_table_grouped[frequency_table_grouped > 0]
+
+    plt.figure(figsize=(10, 10))
+    colors = sns.color_palette("Set3", len(frequency_table_grouped))
+
+    plt.pie(
+        frequency_table_grouped, 
+        labels=frequency_table_grouped.index, 
+        autopct='%1.1f%%', 
+        startangle=90, 
+        colors=colors
+    )
+    plt.xticks(rotation=55)
+
+    plt.title(f'{name}')
+    plt.show()
--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# final-proyect
+# Final Proyect - Retail
+
+## Objetive 
+
+The objective is to predict the demand for products in a shop to optimise inventory levels and reduce losses due to overstocking or out-of-stocks. In addition, to link the demand for these products with the optimisation of the distribution of these products in the warehouse to optimise the preparation/stocking time and maximise sales. 
+
+**Use Cases**
+- Improve stock management.
+- Reduce losses due to unsold products
+- Predict demand in high sales seasons (Christmas, Sales, Holidays) 
+
+## Data Sources
+
+[Kaggle dataset] (https://www.kaggle.com/datasets/hserdaraltan/underwear-data-with-11-tables-and-up-to-100k-rows?select=products.csv)
+
+![Entity-relationship diagram](Datasets/diagrama_entidad_relacion.png)
+
+## Process
+
+### 1. Cleaning
+
+- *8/2/2025*.
+    -  Eliminación de los valores nulos en todos los datasets a excepción de `inventory_transactions` porque al tener tantos valores nulos, quizas pierdo mucha cantidad de ellos. Los valores nulos que tienen son en la cantidad comprada o perdida y muchos de ellos coinciden con los que no tienen registros de compra. 
+    - Unión de datasets como: `Payment Methods` y `Payments`, `Suppliers`y `Purchase Orders` 
+
+### 2. EDA
+
+- *11/2/2025*.
+    - **Estudio clientes.** Demográfico, por ventas, y por fuente de captación. Número de pedidos
+    - **Estudio productos.** Productos y categorías más rentables. Relación costo de envío y el total de ventas por cliente. 
+    - **Estudio ventas.** Total ventas, por categoría. Análisis de venta por tamaño (talla). 
+
+### 3. Visualization
+
+
+## Time Line
+
+| Tuesday | Thursday | Saturday |
+|-----------|-----------|-----------|
+| 4 - Incio  | 6 - Búsqueda dataset  | 8 - Limpieza   |
+| 11 - EDA   | 13 - EDA   | 15 -    |
+| 18 -   | 20 -   | 22 -   |
+| 25 -   | 27 -   | 1 -   |
+| 4 -   | 6 -   | 8 -   |
+| 11 - **Presentacion**  | 13 - **Presentacion**  | 15 - **Presentacion**  |
+
+## Project Structure 📁
+- `datasets/`: contains all clean and original datasets used in this proyect
+- `EDA/`:
+    - `cleaning.ipynb`: jupyter notebook use to clean data
+    - `eda.ipynb`: jupyter notebook use to do eda
+    - `aux_functions.py`: python file where are useful functions were used in other jupyter notebooks
+
+
+
